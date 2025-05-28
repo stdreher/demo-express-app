dffd3b4bae74fceaa343c88d79896bfd68db5515
Steve <steve.turner@gmx.de>
2025-05-28 13:58:22 +0200

    dd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola from Express on Azure!');
+  res.send('Hola from Express on Azure! cadfae<feaGEg');
 });
 
 app.listen(port, () => {
